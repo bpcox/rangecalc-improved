59f5a6f8ff62a7cf81424656819dd8c66ef4f206
Brian Cox <bcox@ucsd.edu>
2014-08-08 02:10:50 -0700

    Formatting of output strings fixed

--- calculator/views.py
+++ calculator/views.py
@@ -46,10 +46,10 @@ def index(request):
             })
             if parsedIPv4Input:
                 output['IPv4'] =str( webcalc.calcIPv4Range(parsedIPv4Input[0],parsedIPv4Input[-1]))
-                output['IPv4multi'] = str(webcalc.maxsizecalcIPv4(parsedIPv4Input,12))
+                output['IPv4multi'] = ', '.join(map(str,webcalc.maxsizecalcIPv4(parsedIPv4Input,12)))
             if parsedIPv6Input:
                 output['IPv6'] = str( webcalc.calcIPv6Range(parsedIPv6Input[0],parsedIPv6Input[-1]))
-                output['IPv6multi'] = str(webcalc.maxsizecalcIPv6(parsedIPv6Input,48))
+                output['IPv6multi'] = ', '.join(map(str,webcalc.maxsizecalcIPv6(parsedIPv6Input,32)))
             if not ('IPv4' in output  or 'IPv6' in output):
                 output['error'] = 'No valid input recieved! Input IPv4 Addresses & Ranges, and IPv6 Addresses only!'
     finaloutput = {'form':form, 'IPv4simple':output}
