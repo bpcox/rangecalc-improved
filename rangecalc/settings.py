7ada89b0c9775180b9ba45f0a62cf6a1f7fca57c
Brian Cox <bcox@ucsd.edu>
2014-08-12 20:25:33 -0400

    update proj. settings

--- rangecalc/settings.py
+++ rangecalc/settings.py
@@ -17,13 +17,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oksb0_#kj4frab1knhe9@7otu%p3(u1rk&0%ld5%l7msdv9lqk'
-
+with open('/webserv/secret_key') as f:
+	SECRET_KEY = f.read().strip()
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+#DEBUG = True
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
+#TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = []
 
 
